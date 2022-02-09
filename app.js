e6027c4aeeaf463e2de34717e7a68a56c1eb1fad
Weston DeVore <westondevorebusiness@gmail.com>
2022-02-09 13:47:34 -0500

    installed dependencies and wrote some broken code for develop

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-//code for app.js
+const Express = require('express');
+const app = Express;
+const bcrypt = require("bcryptjs");
+
+app.use(Express.json());
+
+app.listen(3000, () => {
+    console.log(`[Server]: App is listening on 3000.`);
+});
