0916ec5db612021b511f280bc9bb7fe23cc81903
Guillermo Torres <gt.garcia0515@gmail.com>
2022-02-09 15:53:03 -0500

    Added User Model and Controller

---
+++ model/carCreate.js
@@ -0,0 +1,6 @@
+const { DataTypes } = require('sequelize');
+const db = require('../db');
+
+const Car = db.define('car', {
+    
+})
--- model/index.js
+++ model/index.js
@@ -1,1 +1,8 @@
 //code for index file
+const UserModel = require ('./user')
+const Car = require('./carCreate')
+
+module.exports = {
+    UserModel,
+    Car
+}
