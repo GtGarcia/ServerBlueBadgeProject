239006d4f5ebb355395e5fc06d535188918df711
Guillermo Torres <gt.garcia0515@gmail.com>
2022-02-16 16:37:30 -0500

    Edited our filess

--- middleware/validate-jwt.js
+++ middleware/validate-jwt.js
@@ -11,15 +11,14 @@ const validateJWT = async(req, res, next) => {
             const { authorization } = req.headers
             // console.log("authorization --> ", authorization);
             const payload = authorization
-            ? jwt.verify(authorization, process.env.JWT_SECRET_KEY
+            ? jwt.verify( authorization.includes("Bearer")
+                ? authorization.split(" ")[1]
+                : authorization,
+                process.env.JWT_SECRET_KEY
                 )
                 : undefined;
                 
                 
-                // authorization.includes("Bearer")
-                // ? authorization.split(" ")[1]
-                // : authorization,
-                // process.env.JWT_SECRET_KEY
             if (payload) {
                 let foundUser = await UserModel.findOne({ where: { id: payload.id } });
                 // console.log("foundUser -->", foundUser)
